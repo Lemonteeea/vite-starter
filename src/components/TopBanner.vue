ecd7f871375d4d1f252f6be695cb07e38f9317a0
Lemonteeea <982813440@qq.com>
2022-02-26 23:25:15 +0800

    refactor: 删除无用文件

--- src/components/TopBanner.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="h-12 bg-green-600 flex justify-center items-center text-white">
-    🎉 It's very nice of you to be here.
-  </div>
-</template>
