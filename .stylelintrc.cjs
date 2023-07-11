25b63daa7f6c0386f4eb0be3befe42c5bb799967
Yancy Zhang <lemonteeea@icloud.com>
2023-07-11 21:56:49 +0800

    fix: merge vue-ts

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,17 +1,13 @@
 module.exports = {
   extends: [
     "stylelint-config-standard",
+    "stylelint-config-recommended-vue",
     "stylelint-config-rational-order",
     "stylelint-config-prettier",
   ],
-  plugins: [
-    "stylelint-order",
-    "stylelint-declaration-block-no-ignored-properties",
-  ],
+  plugins: ["stylelint-order"],
   rules: {
-    "plugin/declaration-block-no-ignored-properties": true,
     "color-hex-length": "long",
   },
   ignoreFiles: ["**/*.js", "**/*.jsx", "**/*.tsx", "**/*.ts"],
-  customSyntax: "postcss-html",
 };
